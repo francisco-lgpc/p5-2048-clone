5282d26fc06e0551e01d4ebcc2a7a14b39605441
Francisco Coutinho <francisco.lgpc@gmail.com>
2017-09-16 01:09:10 +0100

    wip - almost done

--- sketch.js
+++ sketch.js
@@ -29,89 +29,118 @@ function draw() {
 }
 
 function keyPressed() {
+	var merged = false
   if (keyCode === LEFT_ARROW) {
-    for (var i = 0; i < totalRows; i++) {
-    	for (var j = 0; j < totalCols; j++) {
-				if (grid[i][j] === null) {
-					for (var k = j + 1; k < totalCols; k++) {
-						if (grid[i][k] !== null) {
-							grid[i][k].update(i, j);
-							break;
+  	for (var times = 0; times < 2; times++) {
+  		if (merged) {
+  			break;
+  		}
+	    for (var i = 0; i < totalRows; i++) {
+	    	for (var j = 0; j < totalCols; j++) {
+					if (grid[i][j] === null) {
+						for (var k = j + 1; k < totalCols; k++) {
+							if (grid[i][k] !== null) {
+								grid[i][k].update(i, j);
+								break;
+							}
 						}
-					}
-				} else if (grid[i][j + 1] && grid[i][j].num === grid[i][j + 1].num) {
-   				var index = tiles.indexOf(grid[i][j + 1]);
-   				if (index > -1) {
- 				   	tiles.splice(index, 1);
- 				   	grid[i][j + 1] = null;
- 				   	grid[i][j].doubleNum();
-					} 			
-    		}
-    	}
-    }
+					} else if (grid[i][j + 1] && grid[i][j].num === grid[i][j + 1].num) {
+	   				var index = tiles.indexOf(grid[i][j + 1]);
+	   				if (index > -1) {
+	 				   	tiles.splice(index, 1);
+	 				   	grid[i][j + 1] = null;
+	 				   	grid[i][j].doubleNum();
+	 				   	merged = true;
+						} 			
+	    		}
+	    	}
+	    }
+	  }
+	  merged = false
   	updateGrid(newTileIndices(1));
   } else if (keyCode === RIGHT_ARROW) {
-    for (var i = 0; i < totalRows; i++) {
-    	for (var j = totalCols - 1; j >= 0; j--) {
-				if (grid[i][j] === null) {
-					for (var k = j - 1; k >= 0; k--) {
-						if (grid[i][k] !== null) {
-							grid[i][k].update(i, j);
-							break;
+  	for (var times = 0; times < 2; times++) {
+  		if (merged) {
+  			break;
+  		}
+	    for (var i = 0; i < totalRows; i++) {
+	    	for (var j = totalCols - 1; j >= 0; j--) {
+					if (grid[i][j] === null) {
+						for (var k = j - 1; k >= 0; k--) {
+							if (grid[i][k] !== null) {
+								grid[i][k].update(i, j);
+								break;
+							}
 						}
-					}
-				} else if (grid[i][j - 1] && grid[i][j].num === grid[i][j - 1].num) {
-   				var index = tiles.indexOf(grid[i][j - 1]);
-   				if (index > -1) {
- 				   	tiles.splice(index, 1);
- 				   	grid[i][j - 1] = null;
- 				   	grid[i][j].doubleNum();
-					} 			
-    		}    		
-    	}
-    }
+					} else if (grid[i][j - 1] && grid[i][j].num === grid[i][j - 1].num) {
+	   				var index = tiles.indexOf(grid[i][j - 1]);
+	   				if (index > -1) {
+	 				   	tiles.splice(index, 1);
+	 				   	grid[i][j - 1] = null;
+	 				   	grid[i][j].doubleNum();
+	 				   	merged = true;
+						} 			
+	    		}    		
+	    	}
+	    }
+	  }
+	  merged = false
   	updateGrid(newTileIndices(1));
   } else if (keyCode === UP_ARROW) {
-  	for (var j = 0; j < totalCols; j++) {
-	    for (var i = 0; i < totalRows; i++) {
-				if (grid[i][j] === null) {
-					for (var k = i + 1; k < totalRows; k++) {
-						if (grid[k][j] !== null) {
-							grid[k][j].update(i, j);
-							break;
+  	for (var times = 0; times < 2; times++) {
+  		if (merged) {
+  			break;
+  		}
+	  	for (var j = 0; j < totalCols; j++) {
+		    for (var i = 0; i < totalRows; i++) {
+					if (grid[i][j] === null) {
+						for (var k = i + 1; k < totalRows; k++) {
+							if (grid[k][j] !== null) {
+								grid[k][j].update(i, j);
+								break;
+							}
 						}
-					}
-				} else if (grid[i + 1][j] && grid[i][j].num === grid[i + 1][j].num) {
-   				var index = tiles.indexOf(grid[i + 1][j]);
-   				if (index > -1) {
- 				   	tiles.splice(index, 1);
- 				   	grid[i + 1][j] = null;
- 				   	grid[i][j].doubleNum();
-					} 			
-    		}    		
-    	}
-    }
+					} else if (grid[i + 1] && grid[i + 1][j] && grid[i][j].num === grid[i + 1][j].num) {
+	   				var index = tiles.indexOf(grid[i + 1][j]);
+	   				if (index > -1) {
+	 				   	tiles.splice(index, 1);
+	 				   	grid[i + 1][j] = null;
+	 				   	grid[i][j].doubleNum();
+	 				   	merged = true;
+						} 			
+	    		}    		
+	    	}
+	    }
+	  }
+	  merged = false
   	updateGrid(newTileIndices(1));
   } else if (keyCode === DOWN_ARROW) {
-  	for (var j = 0; j < totalCols; j++) {
-    	for (var i = totalRows - 1; i >= 0; i--) {
-				if (grid[i][j] === null) {
-					for (var k = i - 1; k >= 0; k--) {
-						if (grid[k][j] !== null) {
-							grid[k][j].update(i, j);
-							break;
+  	for (var times = 0; times < 2; times++) {
+  		if (merged) {
+  			break;
+  		}
+	  	for (var j = 0; j < totalCols; j++) {
+	    	for (var i = totalRows - 1; i >= 0; i--) {
+					if (grid[i][j] === null) {
+						for (var k = i - 1; k >= 0; k--) {
+							if (grid[k][j] !== null) {
+								grid[k][j].update(i, j);
+								break;
+							}
 						}
-					}
-				} else if (grid[i - 1][j] && grid[i][j].num === grid[i - 1][j].num) {
-   				var index = tiles.indexOf(grid[i - 1][j]);
-   				if (index > -1) {
- 				   	tiles.splice(index, 1);
- 				   	grid[i - 1][j] = null;
- 				   	grid[i][j].doubleNum();
-					} 			
-    		}     		
-    	}
-    }
+					} else if (grid[i - 1] && grid[i - 1][j] && grid[i][j].num === grid[i - 1][j].num) {
+	   				var index = tiles.indexOf(grid[i - 1][j]);
+	   				if (index > -1) {
+	 				   	tiles.splice(index, 1);
+	 				   	grid[i - 1][j] = null;
+	 				   	grid[i][j].doubleNum();
+	 				   	merged = true;
+						} 			
+	    		}     		
+	    	}
+	    }
+	  }
+	  merged = false
     updateGrid(newTileIndices(1));
   }
 }
